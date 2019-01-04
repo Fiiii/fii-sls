0383510640c26d2ac8cc64fcdbe5f491094aa0c8
Fiiii <artur.figiel@netguru.co>
2019-01-04 13:08:29 +0100

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack')
+const slsw = require('serverless-webpack')
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  externals: [
+    /aws-sdk/
+  ],
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: true,
+      mangle: process.env.NODE_ENV === 'production',
+      beautify: process.env.NODE_ENV !== 'production',
+      output: {
+        comments: process.env.NODE_ENV !== 'production'
+      },
+      sourceMap: false
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/, /test/],
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            [
+              'env',
+              {
+                target: { node: 8.10 }, // Node version on AWS Lambda
+                useBuiltIns: true,
+                modules: false,
+                loose: true
+              }
+            ]
+          ]
+        }
+      }
+    ]
+  }
+}
