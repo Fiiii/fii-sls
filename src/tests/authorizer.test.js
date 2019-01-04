943194bb43167e45885021be844f43e604588c36
Fiiii <artur.figiel@netguru.co>
2019-01-04 18:12:46 +0100

    Tests migration

--- src/tests/authorizer.test.js
+++ src/tests/authorizer.test.js
@@ -1,19 +1,16 @@
 import { isFunction } from 'lodash'
-import { init } from '../../lib/initFunction'
+import { init } from '../lib/initFunction'
 import { Container } from 'aurelia-dependency-injection'
-import apiAuthorizer from './'
+import apiAuthorizer from '../handlers/authorizer'
 
-jest.mock('../../lib/initFunction', () => { return { init: jest.fn(() => () => { }) } })
+jest.mock('../lib/initFunction', () => { return { init: jest.fn(() => () => { }) } })
 
 describe('Authorizer', () => {
-  it('Dummy test', () => {
-    expect(1).toEqual(1)
-  })
-
   it('should register ApiAuthorizerHandler inside DI container', () => {
-    const initContainerFunc = init.mock.calls[0][2]
+    const initContainerFunction = init.mock.calls[0][2]
     const container = new Container()
-    initContainerFunc(container)
+
+    initContainerFunction(container)
     expect(container.getResolver('ApiAuthorizerHandler')).toBeDefined()
   })
 
