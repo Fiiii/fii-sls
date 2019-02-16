a8218203629268a59f8dd6ba8ef0b9c66e05dff0
Fiiii <artur.figiel@netguru.co>
2019-02-16 11:19:00 +0100

    Add dbconnector mockup

--- src/handlers/authorizer/index.js
+++ src/handlers/authorizer/index.js
@@ -1,6 +1,8 @@
 import ApiAuthorizerHandler from './authorizerHandler'
+import { getConnection } from '/opt/dbconnection/dbconnector'
 
 const handler = async (event) => {
+  console.log(await getConnection())
   try {
     console.log('Auth')
   } catch (error) {
