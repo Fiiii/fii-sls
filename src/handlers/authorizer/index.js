3ad320cb9f415609421dcb8f20ae70103bde295a
Fiiii <artur.figiel@netguru.co>
2019-03-06 15:56:32 +0100

    Clean up

--- src/handlers/authorizer/index.js
+++ src/handlers/authorizer/index.js
@@ -1,12 +1,7 @@
-import ApiAuthorizerHandler from './authorizerHandler'
-import { getConnection } from '/opt/dbconnection/dbconnector'
-
 const handler = async (event) => {
-  console.log(await getConnection())
   try {
-    console.log('Auth')
   } catch (error) {
-    throw new Error('Unauthorized')
+    throw new Error(error)
   }
 }
 
